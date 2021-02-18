36f522a0fdc71db03bc5e69e605b47533ca402dd
Marisa Nesmith <marisanesmith@Marisas-iMac.local>
2021-02-18 16:03:59 -0500

    updated index.js

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# README_Generator
+# New Project
+MIT
+
+it is easy to use
+
+Table of Contents
+[Installation](Installation)
+[Usage](Usage)
+[Contribution](Contribution)
+[Tests](Test)
+[Questions](Questions)
+
+## Installation
+the installation guide is downloadable
+
+## Usage
+it is very easy to use
+
+## Contribution
+just use the application
+
+## Tests
+here is how you will use the application
+
+## Questions
+
+Please contact me via email if you have any questions about this application.
+
+GitHub Profile: [github.com/marisa](github.com/marisa)
+
+Email: [marisa@gmail](marisa@gmail)
--- index.js
+++ index.js
@@ -4,95 +4,104 @@ const fs = require("fs");
 const path = require("path");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const generateReadme = (answers) =>
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+`# ${answers.title}
+License: ${answers.license}
 
-// TODO: Create a function to initialize app
-function init() {}
+${answers.description}
 
-// Function call to initialize app
-init();
+Table of Contents
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [Contribution](#Contribution)
+* [Tests](#Test)
+* [License](#License)
+* [Questions](#Questions)
 
-const generateHTML = (answers) =>
-  `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">${answers.title}</h1>
-    <p class="lead">${answers.description}.</p>
-    <h3>Example heading <span class="badge badge-secondary">${answers.license}</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${answers.githubName}</li>
-      <li class="list-group-item">Email: ${answers.email}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## Contribution
+* ${answers.contribution}
+
+## Tests
+* ${answers.test}
+
+## License
+${answers.license}
+You can find out more about the ${answers.license} license on the open source page [here](https://www.opensource.org/licenses/${answers.license})
+
+## Questions
+
+Please contact me via email if you have any questions about this application.
+
+* GitHub Profile: [github.com/${answers.githubName}](github.com/${answers.githubName})
+
+* Email: [${answers.email}](${answers.email})`;
 
 inquirer
 .prompt([
     {
-      type: 'input',
-      name: 'title',
-      message: 'What is the title of your project?',
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
     },
     {
-      type: 'input',
-      name: 'description',
-      message: 'How would you describe your project to other users?',
+    type: 'input',
+    name: 'description',
+    message: 'How would you describe your project to other users?',
     },
     {
-      type: 'input',
-      name: 'installation',
-      message: 'Explain how you would install this project',
+    type: 'input',
+    name: 'installation',
+    message: 'Explain how you would install this project',
     },
     {
-      type: 'input',
-      name: 'usage',
-      message: 'Explain how to use your application',
+    type: 'input',
+    name: 'usage',
+    message: 'Explain how to use your application',
     },
     {
-      type: 'input',
-      name: 'contribution',
-      message: 'Include a guide on how others could contribute to your application or open source project.',
+    type: 'input',
+    name: 'contribution',
+    message: 'Include a guide on how others could contribute to your application or open source project.',
     },
     {
-      type: 'input',
-      name: 'test',
-      message: 'Create a guideline of instructions for how to test the application',
+    type: 'input',
+    name: 'test',
+    message: 'Create a guideline of instructions for how to test the application',
     },
     {
-        type: 'list',
-        name: 'license',
-        message: 'Select from the list below which license you would like to use for your application',
-        choice: ["MIT", "GPLv2", "Apache", "GPLv3", "BSD 3-clause"]
-      },
-      {
-        type: 'input',
-        name: 'githubName',
-        message: 'Enter your GitHub username',
-      },
-      {
-        type: 'input',
-        name: 'email',
-        message: 'Enter your email address and a brief description with instructions on how someone can reach you with additional questions about the application.',
-      },
-  ])
-  .then((answers) => {
-    const htmlPageContent = generateHTML(answers);
+    type: 'list',
+    name: 'license',
+    message: 'Select from the list below which license you would like to use for your application',
+    choices: ["MIT", "GPLv2", "Apache", "GPLv3", "BSD 3-clause"]
+    },
+    {
+    type: 'input',
+    name: 'githubName',
+    message: 'Enter your GitHub username',
+    },
+    {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address and a brief description with instructions on how someone can reach you with additional questions about the application.',
+    },
+])
+.then((answers) => {
+    const createReadme = generateReadme(answers);
 
-    fs.writeFile('index.html', htmlPageContent, (err) =>
-      err ? console.log(err) : console.log('Successfully created index.html!')
+    fs.writeFile('README.md', createReadme, (err) =>
+    err ? console.log(err) : console.log('Successfully created README file!')
     );
-  });
+});
+
+  // TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
